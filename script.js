784d7a3a97c5e363d85b99d675948e9be3927f80
Denis <padurariu_denis@yahoo.com>
2023-04-08 14:16:12 +0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,107 @@
+const apiKey = "920f4407688f95c37a001112dac724e1";
+const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search-bar input");
+const searchButton = document.querySelector(".search-bar button");
+const weatherIcon = document.querySelector(".weather-icon");
+let latitude;
+let longitude;
+
+const successCallback = (position) => {
+    console.log(position.coords);
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log(longitude, latitude);
+    getInitialWeather(latitude, longitude);
+};
+
+const errorCallback = (error) => {
+    console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback); //functie principala locatie
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temperature").innerHTML =
+            Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML =
+            data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "img/clouds.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "img/rain.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "img/clear.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "img/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "img/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+        console.log(data);
+    }
+}
+
+async function getInitialWeather(lat, long) {
+    const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?lat={" +
+            lat +
+            "}&lon={" +
+            long +
+            "}" +
+            `&appid=${apiKey}`
+    );
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        var data = await response.json();
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temperature").innerHTML =
+            Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML =
+            data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "img/clouds.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "img/rain.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "img/clear.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "img/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "img/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+        console.log(data);
+    }
+}
+
+searchButton.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
+
 const toggle = document.getElementById("toggleDark");
 const body = document.querySelector("body");
 
